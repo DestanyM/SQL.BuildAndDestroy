31309e36260190389b109a4bb083220c8cb3ac65
DestanyM <84419205+DestanyM@users.noreply.github.com>
2023-08-05 12:39:26 -0400

    Update exercise8.sql

---
+++ answers/exercise8.sql
@@ -0,0 +1,2 @@
+create table students(StudentName VARCHAR(255) not null, Address VARCHAR(255) not null, City Varchar(255) not null, PostalCode VARCHAR(255) not null, Country VARCHAR(255) not null);
+insert into students(StudentName, Address, City, PostalCode, Country) values ('Jane Doe', '57 Union St', 'Glasgow', 'G13RB', 'Scotland');
